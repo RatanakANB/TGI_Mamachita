642397a2f21fa8bf519da1f211216a54d09e27a6
srunchanmonika <srunchanmonika59@gmail.com>
2025-05-23 17:16:59 +0700

    Initialize customer.h

---
+++ dashboard/customer.h
@@ -0,0 +1,24 @@
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+
+#include <stdio.h>
+#include <string.h>
+
+int customerMenu() {
+    printf("\n=== CUSTOMER DASHBOARD ===\n");
+    printf("1. Browse shops\n");
+    printf("2. View cart\n");
+    printf("3. Add funds\n");
+    printf("4. Checkout\n");
+    printf("5. Back\n");
+
+}
+
+int customer_dashboard() {
+
+
+    return customerMenu();
+}
+
+
+#endif
